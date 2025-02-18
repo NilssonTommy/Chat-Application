d3867203dfbeaaf65892f8a7dbcbae3975ea85ec
Jesper Nyberg <jespernyberg@Jespers-MacBook-Pro.local>
2025-02-18 11:31:23 +0100

    test

--- src/main/java/com/example/testclass.java
+++ src/main/java/com/example/testclass.java
@@ -1,6 +1,5 @@
 package com.example;
 import java.io.File;
-
 import javax.imageio.ImageIO;
 /**
  * Testklass för att testa guin.
@@ -14,28 +13,10 @@ public class testclass{
         ChatroomGUI gui = builder.getResult();
         gui.getUserwindow().addText("hej");
         gui.getUserwindow().addText("test");
-        for(int i=0; i<15; i++){/*
-            gui.getUserwindow().addText("hej");
-            gui.getUserwindow().addText("test");
-            gui.getUserwindow().addText("hej");
-            gui.getUserwindow().addText("test");
-            gui.getUserwindow().addText("hej");
-            gui.getUserwindow().addText("test");
-            gui.getUserwindow().addText("hej");
-            gui.getUserwindow().addText("test");
-            gui.getChatwindow().addText("11111");
-            gui.getChatwindow().addText("22222");
-            gui.getChatwindow().addText("33333");
-            gui.getChatwindow().addText("44444");
-            gui.getChatwindow().addText("55555");
-            gui.getChatwindow().addText("66666");
-            gui.getChatwindow().addText("77777");
-            gui.getChatwindow().addText("88888");
-            gui.getChatwindow().addText("99999");*/
+        for(int i=0; i<15; i++){
             gui.getUserwindow().addText("hej");
             gui.getUserwindow().addText("test");
             gui.getChatwindow().addText("101010101010101010");
-            //gui.chatscroll.getVerticalScrollBar().setValue(gui.chatscroll.getVerticalScrollBar().getMaximum());
             Thread.sleep(5);
 
             gui.chatwindow.revalidate();
@@ -43,6 +24,7 @@ public class testclass{
         }
         gui.getChatwindow().addImg(ImageIO.read(new File("/Users/jespernyberg/Pictures/icon.png")));
         gui.getChatwindow().addImg(ImageIO.read(new File("/Users/jespernyberg/Pictures/icon.png")));
+        gui.chatwindow.revalidate();
         gui.sendbutton.addActionListener(e->{
             gui.chatwindow.revalidate();
             gui.userwindow.revalidate();
